e6d202f4a3e67d59fe81d5e6f2660dd5439ee61a
白彬涵 <MRWhite@WhiteMac.local>
2017-07-11 14:15:49 +0800

    绘图活动的三个圆圈，bug：如何实现layer自己覆盖自己

--- SomePractice/CAShapeLayer/CircleAnimation/CircleAnimation/DrewCircleView.swift
+++ SomePractice/CAShapeLayer/CircleAnimation/CircleAnimation/DrewCircleView.swift
@@ -8,40 +8,99 @@
 
 import UIKit
 
+let redColor_alpha = UIColor.init(red: 1.0, green: 0.2, blue: 0.2, alpha: 0.3).cgColor
+let greenColor_alpha = UIColor.init(red: 0.2, green: 1.0, blue: 0.2, alpha: 0.3).cgColor
+let blueColor_alpha = UIColor.init(red: 0.2, green: 0.2, blue: 1.0, alpha: 0.3).cgColor
+
+let redColor = UIColor.init(red: 1.0, green: 0.2, blue: 0.2, alpha: 1.0).cgColor
+let greenColor = UIColor.init(red: 0.2, green: 1.0, blue: 0.2, alpha: 1.0).cgColor
+let blueColor = UIColor.init(red: 0.2, green: 0.5, blue: 1.0, alpha: 1.0).cgColor
+
 public class DrewCircleView: UIView, CAAnimationDelegate {
 
+    var redCircleLayer: CAShapeLayer?
+    var greenCircleLayer: CAShapeLayer?
+    var blueCircleLayer: CAShapeLayer?
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override public func draw(_ rect: CGRect) {
         // Drawing code
         super.draw(rect)
-        
+        let length = min(rect.size.width, rect.size.height)
         // 创建Layer
+        // 绘制底层Layers
+        self.layer.addSublayer(circleLayerWith(color: redColor_alpha, radius: length/2 - length/8, viewWidth: length, percent: 1.0))
+        self.layer.addSublayer(circleLayerWith(color: greenColor_alpha, radius: length/2 - length/4, viewWidth: length, percent: 1.0))
+        self.layer.addSublayer(circleLayerWith(color: blueColor_alpha, radius: length/2 - length*3/8, viewWidth: length, percent: 1.0))
+        
+        // 绘制上层Layers并添加动画
+        redCircleLayer = circleLayerWith(color: redColor, radius: length/2 - length/8, viewWidth: length, percent: 0.8).addCircleAnimation(duration: 3.0).1
+        greenCircleLayer = circleLayerWith(color: greenColor, radius: length/2 - length/4, viewWidth: length, percent: 0.7).addCircleAnimation(duration: 3.0).1
+        blueCircleLayer = circleLayerWith(color: blueColor, radius: length/2 - length*3/8, viewWidth: length, percent: 0.9).addCircleAnimation(duration: 3.0).1
+        
+        
+        // Create shadow layer
+        let shadowLayer = CALayer()
+        shadowLayer.frame = rect
+        shadowLayer.shadowColor = UIColor.black.cgColor
+        shadowLayer.shadowOffset = CGSize.zero
+        shadowLayer.shadowRadius = 10
+        shadowLayer.shadowOpacity = 1.0
+        shadowLayer.backgroundColor = UIColor.clear.cgColor
+        shadowLayer.insertSublayer(redCircleLayer!, at: 0)
+        
+        // Shadow path animation
+        let shadowPathAnimation: CABasicAnimation = CABasicAnimation(keyPath: "shadowPath")
+        shadowPathAnimation.fromValue = 0.0
+        shadowPathAnimation.toValue = redCircleLayer?.path
+        shadowLayer.add(shadowPathAnimation, forKey: "shadowPathAnimation")
+        
+        // 添加动画
+        self.layer.addSublayer(shadowLayer)
+        self.layer.addSublayer(greenCircleLayer!)
+        self.layer.addSublayer(blueCircleLayer!)
+    }
+    
+    
+    /// 创建一个圆圈Layer，但是还没有动画
+    ///
+    /// - Parameters:
+    ///   - color: 颜色
+    ///   - radius: 半径
+    ///   - viewWidth: 视图宽度
+    ///   - percent: 百分比
+    /// - Returns: 返回一个CAShapeLayer
+    private func circleLayerWith(color: CGColor, radius: CGFloat, viewWidth: CGFloat, percent: CGFloat) -> CAShapeLayer {
         let layer = CAShapeLayer()
         layer.fillColor = UIColor.clear.cgColor
-        layer.lineWidth = 20.0
+        layer.lineWidth = viewWidth/8 - 10
         layer.lineCap = kCALineCapRound
         layer.lineJoin = kCALineJoinRound
-        layer.strokeColor = UIColor.red.cgColor
-        self.layer.addSublayer(layer)
-        
-        // 创建贝塞尔路径
-        let length = min(rect.size.width, rect.size.height)
-        let path = UIBezierPath.init(arcCenter: CGPoint.init(x: length/2, y: length/2), radius: length/2 - 20, startAngle: 0, endAngle: CGFloat.pi*2, clockwise: false)
-        
-        // 关联贝塞尔路径和layer
+        layer.strokeColor = color
+        let path = UIBezierPath.init(arcCenter: CGPoint.init(x: viewWidth/2, y: viewWidth/2),
+                                     radius: radius,
+                                     startAngle: -CGFloat.pi*0.5,
+                                     endAngle: CGFloat.pi*2*percent-CGFloat.pi*0.5,
+                                     clockwise: true)
         layer.path = path.cgPath
+        return layer
+    }
+    
+    public func drewThreeCircleWith(redPercent: Double, greenPercent: Double, bluePercent: Double) {
         
-        
+    }
+}
+
+
+extension CAShapeLayer {
+    func addCircleAnimation(duration: TimeInterval) -> (CAAnimation, CAShapeLayer) {
         // 创建动画
         let animation = CABasicAnimation.init(keyPath: "strokeEnd")
         animation.fromValue = 0.0
         animation.toValue = 1.0
-        layer.autoreverses = false
-        animation.duration = 3.0
-        animation.delegate = self
-        
-        layer.add(animation, forKey: "strokeEndAnimation")
+        animation.duration = duration
+        self.add(animation, forKey: "strokeEndAnimation")
+        return (animation, self)
     }
-    
 }
--- SomePractice/CAShapeLayer/Demo/Demo/ViewController.swift
+++ SomePractice/CAShapeLayer/Demo/Demo/ViewController.swift
@@ -11,11 +11,31 @@ import CircleAnimation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circleView: DrewCircleView!
+    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let view = DrewCircleView()
-        view.frame = CGRect.init(x: 100, y: 100, width: 200, height: 200)
-        self.view.addSubview(view)
+        /*
+        let layer = CALayer()
+        
+        layer.frame = CGRect(x: 75, y: 75, width: 150, height: 150)
+        layer.backgroundColor = UIColor.darkGray.cgColor
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowRadius = 5
+        layer.shadowOpacity = 1
+        
+        let contactShadowSize: CGFloat = 20
+        let shadowPath = CGPath(ellipseIn: CGRect(x: -contactShadowSize,
+                                                  y: -contactShadowSize * 0.5,
+                                                  width: layer.bounds.width + contactShadowSize * 2,
+                                                  height: contactShadowSize),
+                                transform: nil)
+        
+        layer.shadowPath = shadowPath
+        
+        self.view.layer.addSublayer(layer)
+ */
     }
 
     override func didReceiveMemoryWarning() {
