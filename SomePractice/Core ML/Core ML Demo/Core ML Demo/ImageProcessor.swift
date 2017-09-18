66069803765013277e6ed54241b707b17d35d6dc
白彬涵 <MRWhite@WhiteMac.local>
2017-09-18 16:28:01 +0800

    commit

---
+++ SomePractice/Core ML/Core ML Demo/Core ML Demo/ImageProcessor.swift
@@ -0,0 +1,49 @@
+//
+//  File.swift
+//  BasicCoreML
+//
+//  Created by Brian Advent on 09.06.17.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import CoreVideo
+import UIKit
+
+struct ImageProcessor {
+    static func pixelBuffer (forImage image:CGImage) -> CVPixelBuffer? {
+        
+        
+//        let frameSize = CGSize(width: image.width, height: image.height)
+        let frameSize = CGSize(width: 224, height: 224)
+        
+        var pixelBuffer:CVPixelBuffer? = nil
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(frameSize.width), Int(frameSize.height), kCVPixelFormatType_32BGRA , nil, &pixelBuffer)
+        
+        if status != kCVReturnSuccess {
+            return nil
+            
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags.init(rawValue: 0))
+        let data = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue)
+        let context = CGContext(data: data, width: Int(frameSize.width), height: Int(frameSize.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: bitmapInfo.rawValue)
+        
+        
+        context?.draw(image, in: CGRect(x: 0, y: 0, width: image.width, height: image.height))
+        
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+        
+    }
+    
+    static func imageScaledToSize(sourceImage: UIImage, size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContext(size)
+        sourceImage.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+}
