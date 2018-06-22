42af4449adeb07d143e6dced1569fd97b37f9cf2
baibinhan <baibinhan@cmiot.chinamobile.com>
2018-06-22 16:00:01 +0800

    增加一道笔试题目

---
+++ SomePractice/TW_Trains/src/main/java/com.thoughtworks.trains/Application.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.trains;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ SomePractice/iOS Search/iOSSearchTest/iOSSearchTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSSearchTest
+//
+//  Created by 白彬涵 on 2017/10/30.
+//  Copyright © 2017年 CMIOT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
